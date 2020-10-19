5ad2341e3f3e0808d713b698c850f617a35e35c8
JaxonHarvey <42556138+JaxonHarvey@users.noreply.github.com>
2020-10-19 10:58:47 -0500

    Update template-contactus.php

--- themes/vrctheme/template-contactus.php
+++ themes/vrctheme/template-contactus.php
@@ -17,6 +17,38 @@ Template Name: Contact Us
         <div class="col-lg-6">
         
             <p>This is where the contact form goes</p>
+            <form action="contact.php" method="post">
+  <div class="elem-group">
+    <label for="name">Your Name</label>
+    <input type="text" id="name" name="visitor_name" placeholder="John Doe" pattern=[A-Z\sa-z]{3,20} required>
+  </div>
+  <div class="elem-group">
+    <label for="email">Your E-mail</label>
+    <input type="email" id="email" name="visitor_email" placeholder="john.doe@email.com" required>
+  </div>
+  <div class="elem-group">
+    <label for="category-selection">Choose Concerned Category</label>
+    <select id="category-selection" name="concerned_category" required>
+        <option value="">Select a Category</option>
+        <option value="physical_health">Physical Health</option>
+        <option value="mental_health">Mental Health</option>
+        <option value="spiritual_health">Spiritual Health</option>
+        <option value="economic_health">Economic Health</option>
+        <option value="education">Education</option>
+        <option value="social_services">Social Services</option>
+        <option value="technical_support">Technical Support</option>
+    </select>
+  </div>
+  <div class="elem-group">
+    <label for="title">Reason For Contacting Us</label>
+    <input type="text" id="title" name="email_title" required placeholder="Unable to Reset my Password" pattern=[A-Za-z0-9\s]{8,60}>
+  </div>
+  <div class="elem-group">
+    <label for="message">Write your message</label>
+    <textarea id="message" name="visitor_message" placeholder="Say whatever you want." required></textarea>
+  </div>
+  <button type="submit">Send Message</button>
+</form>
         
         </div>
 
