ec7b69f1585fe43383e265a64e7d59c4749e2add
Jonathan Steigleman <jonathansteigleman@yahoo.com>
2020-10-20 16:47:32 -0500

    added the footer

--- themes/vrctheme/footer.php
+++ themes/vrctheme/footer.php
@@ -4,3 +4,85 @@
     <?php wp_footer();?>
 </body>
 </html>
+
+<footer class="page-footer font-small blue-grey lighten-5">
+
+
+  <div style="background-color: #F5F5F5;">
+  <div class="row d-flex align-items-center">
+  <div class="container text-center text-md-left mt-5">
+
+    <div class="row mt-3 dark-grey-text">
+
+      <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
+
+        <h6 class="text-uppercase font-weight-bold">Polk Coounty Connections Center</h6>
+        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+
+      </div>
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Pages</h6>
+        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <a class="dark-grey-text" href="http://pccc.test:8888/economic-health/">Economic Health</a>
+        </p>
+        <p>
+          <a class="dark-grey-text" href="http://pccc.test:8888/social-services/">Social Services</a>
+        </p>
+        <p>
+          <a class="dark-grey-text" href="http://pccc.test:8888/education/">Education</a>
+        </p>
+        <p>
+          <a class="dark-grey-text" href="http://pccc.test:8888/mental-health/">Mental Health</a>
+        </p>
+        <p>
+        	<a class="dark-grey-text" href="http://pccc.test:8888/spiritual-health/">Spiritual Health</a>
+        </p>
+        <p>
+        	<a class="dark-grey-text" href="http://pccc.test:8888/physical-health/">Physical Health</a>
+        </p>
+
+
+      </div>
+
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+        <h6 class="text-uppercase font-weight-bold">Other links</h6>
+        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <a class="dark-grey-text" href="#!">Request to be added to list</a>
+        </p>
+        <p>
+          <a class="dark-grey-text" href="#!">Make a change to a listing</a>
+        </p>
+        <p>
+          <a class="dark-grey-text" href="#!">Upcoming Events</a>
+        </p>
+        <p>
+          <a class="dark-grey-text" href="#!">Help</a>
+        </p>
+
+      </div>
+
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+        <h6 class="text-uppercase font-weight-bold">Contact Us</h6>
+        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <i class="fas fa-home mr-3"></i> Anchorage Alaska, AK 99504, US</p>
+        <p>
+          <i class="fas fa-envelope mr-3"></i> homeboy@example.com</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i> +01 867-5309</p>
+        <p>
+          <i class="fas fa-print mr-3"></i> + 01 867-5309</p>
+
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</footer>
