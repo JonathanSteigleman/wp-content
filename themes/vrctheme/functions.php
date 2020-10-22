6065253bc531b431cf19fe2990e3f7fa32ecaf8a
kalicrockett <71088446+kalicrockett@users.noreply.github.com>
2020-10-22 17:20:37 -0500

    Added Custom Logo

--- themes/vrctheme/functions.php
+++ themes/vrctheme/functions.php
@@ -63,3 +63,19 @@ register_nav_menus(
     )
 
 );
+
+//Custom logo
+add_theme_support( 'custom-logo' );
+
+function themename_custom_logo_setup() {
+    $defaults = array(
+    'height'      => 50,
+    'width'       => 100,
+    'flex-height' => true,
+    'flex-width'  => true,
+    'header-text' => array( 'site-title', 'site-description' ),
+   'unlink-homepage-logo' => true, 
+    );
+    add_theme_support( 'custom-logo', $defaults );
+   }
+   add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
