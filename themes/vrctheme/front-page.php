c03eaa0e8ec7c7c97d3f392e43ca8adb25a0da95
kalicrockett <71088446+kalicrockett@users.noreply.github.com>
2020-11-02 12:37:17 -0600

    Front Page Hero with Announcements added with ACF

--- themes/vrctheme/front-page.php
+++ themes/vrctheme/front-page.php
@@ -1,8 +1,23 @@
 <!-- the front page will use the secondar header -->
-<?php get_header('secondary');?>
+<?php
+    get_header('secondary');
+    acf_add_local_field_group('Home');
+?>
+
+<div class="jumbotron-fluid" style="background:  linear-gradient(rgba(112, 112, 112, 0.45), rgba(245, 245, 245, 0.5)), url(<?php the_field('hero_image');?>); background-size:cover;">
+    <div class="row align-items-end">
+        <div class="col-md-auto" id="announcements">
+        <h2><?php the_field('hero_heading');?></h2>
+        <p class="mt-2"><?php the_field('hero_content');?></p>
+        <a href="<?php the_field('hero_button');?>"><button type="button" class="btn btn-primary mt-2">Read More</button></a>
+        </div>
+    </div>
+</div>
+
 
 <div class="page-wrap">
 <div class="container">
+
 
     <h1><?php the_title();?></h1>
 
