c0f34e43c3b08e04239f246af7da7f9dded0207f
kalicrockett <71088446+kalicrockett@users.noreply.github.com>
2020-12-02 19:44:00 -0600

    added comments to page.php

--- themes/vrctheme/page.php
+++ themes/vrctheme/page.php
@@ -1,12 +1,23 @@
-<?php get_header();?>
+<?php 
+//////////////////////////////////////////////////////////////////////////////
+// The generic page template
+// This simply calls the title of the page and any content added by the user
+// Uses the main header
+//////////////////////////////////////////////////////////////////////////////
+get_header();
+?>
 
 <div class="page-wrap">
 <div class="container">
-
+    <!-- get the title of the page -->
     <h1><?php the_title();?></h1>
-
+    <!-- get the content of the page -->
     <?php get_template_part('includes/section', 'content');?>
 
 </div>
 </div>
-<?php get_footer();?>
+
+<?php 
+// calls the footer
+get_footer();
+?>
