4980e6db84e4f103b855e1c11616641ef93add4f
JaxonHarvey <42556138+JaxonHarvey@users.noreply.github.com>
2021-02-28 14:28:05 -0600

    Update template-individual-page.php

--- themes/vrctheme/template-individual-page.php
+++ themes/vrctheme/template-individual-page.php
@@ -8,30 +8,32 @@ Template Name: Individual Page
 <!-- Header for template -->
 
 <div class="header">
-    <h2>Health</h2>
-  </div>
+  <h2>Health</h2>
+</div>
   
 <div class="row">
-    <div class="leftcolumn">
-        <div class="card">
-         <h2>Business Name Here</h2>
+  <div class="leftcolumn">
+      <div class="card">
+        <h2>Business Name Here</h2>
         <!-- <h5>Title description, Dec 7, 2017</h5> --> 
         <!-- Could be the address portion underneath business name-->
         <div class="img" style="height:200px;">Image</div>
         <!-- Logo for the business? -->
         <p>Some text..</p>
         <!-- Descriptiopn of the business under logo? -->
-    </div>
+      </div>
 
-    <div class="rightcolumn">
-        <div class="card">
-          <h2>Business Person Here</h2>
-          <!-- Name for the person who they contact -->
-          <div class="fimg" style="height:100px;">Image</div>
-          <!-- Image of the person to contact? -->
-          <p>text detailing phone number and position at business and such...</p>
-          <!-- further description of person -->
-        </div>
+  <div class="rightcolumn">
+      <div class="card">
+        <h2>Business Person Here</h2>
+        <!-- Name for the person who they contact -->
+        <div class="fimg" style="height:100px;">Image</div>
+        <!-- Image of the person to contact? -->
+        <p>text detailing phone number and position at business and such...</p>
+        <!-- further description of person -->
+      </div>
+    </div>
+  </div>
 </div>
 
 
