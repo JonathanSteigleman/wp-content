377712ace77b4962dbe35ee5d573fe258d51429a
JaxonHarvey <42556138+JaxonHarvey@users.noreply.github.com>
2021-02-28 16:46:30 -0600

    Update again

--- themes/vrctheme/template-individual-page.php
+++ themes/vrctheme/template-individual-page.php
@@ -17,7 +17,7 @@ Template Name: Individual Page
         <h2>Business Name Here</h2>
         <!-- <h5>Title description, Dec 7, 2017</h5> --> 
         <!-- Could be the address portion underneath business name-->
-        <div class="business_logo" style="height:100px;">Image</div>
+        <div class="fakeImage" style="height:100px;">Image</div>
         <!-- Logo for the business? -->
         <p>Some text..</p>
         <!-- Descriptiopn of the business under logo? -->
@@ -27,7 +27,7 @@ Template Name: Individual Page
       <div class="card">
         <h2>Business Person Here</h2>
         <!-- Name for the person who they contact -->
-        <div class="person_image" style="height:50px;">Image</div>
+        <div class="fakeImage" style="height:50px;">Image</div>
         <!-- Image of the person to contact? -->
         <p>text detailing phone number and position at business and such...</p>
         <!-- further description of person -->
