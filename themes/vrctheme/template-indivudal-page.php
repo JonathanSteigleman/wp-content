c9bed77b18fb69454cf54fd373e6d6e4de0465f9
JaxonHarvey <42556138+JaxonHarvey@users.noreply.github.com>
2021-03-22 19:03:11 -0500

    updating changes from Last week

--- themes/vrctheme/template-indivudal-page.php
+++ themes/vrctheme/template-indivudal-page.php
@@ -4,48 +4,52 @@ Template Name: Individual Page
 Jaxon Harvey
 */
 get_header();
+acf_add_local_field_group('Category_Description');
 ?>
 
 
 <div class="page-wrap">
 <div class="container">
 <h1><?php the_title();?></h1>
-    <?php get_template_part('includes/section', 'content');?>
+    <!-- ?php get_template_part('includes/section', 'content');? -->
 
     <!-- Row 1 of Categories -->
     <div class="row pb-4">
             <?php
                 // Initialize catNum to 1
                 // This will be used to incriment through categories in the while loop
-                $catNum = 1;
 
-                // while we are at a category number under 4
-                while ( $catNum < 4):
-                // set the value of $category equal to category_ plus the value of $catNum
-                $category = "category_".$catNum;
+                // $category_1 = "category_1";
+                // $category_2 = "category_2";
+                
                 // set the current category to the ACF value with the name equal to $category
-                $currentCat = get_field($category);
+                $category_1 = get_field("category_1");
+                $category_2 = get_field("category_2");
 
                 //if there is content in this category
-                if ($currentCat): ?>
+                if ($category_1): ?>
                     <div class="col-lg-6 col-md-auto col-sm-auto">
                         <!-- get title added in this category -->
-                        <h4><?php echo $currentCat['title'];?></h4>
-                        <?php echo $currentCat['category_description'];?>
-                    </div>
-                    
+                        <h4><?php echo $category_1['title'];?></h4>
+                        <?php echo $category_1['category_description'];?>
+                    </div> <!-- End Col -->
+                <?php endif ?> <!-- End If -->
+
+
+                <?php
+                if ($category_2): ?>
                     <div class="col-lg-6 col-md-auto col-sm-auto">
                         <div class="card">
 
                             <div class="card-body">
-                                <h4><?php echo $currentCat['contact_info'];?></h4>
-                                <img class="contact_image" src="<?php echo esc_url($currentCat['image']);?>"/>
+                                <h4><?php echo $category_2['contact_info'];?></h4>
+                                <img class="contact_image" src="<?php echo esc_url($category_2['image']);?>"/>
                                 <!-- get the URL of the image added in this category -->
 
                                 <ul> <!-- Unordered list to provide spacing -->
-                                <li> <?php echo $currentCat['name'];?> </li>
-                                <li> <?php echo $currentCat['address'];?> </li>
-                                <li> <?php echo $currentCat['number'];?> </li>
+                                <li> <?php echo $category_2['name'];?> </li>
+                                <li> <?php echo $category_2['address'];?> </li>
+                                <li> <?php echo $category_2['number'];?> </li>
                                 </ul> <!-- Unordered list to provide spacing -->
 
                             </div><!-- end card body div -->
@@ -53,12 +57,6 @@ get_header();
                     </div><!-- End col -->
                     <!-- end the if statement -->
                 <?php endif ?>
-
-                    <!-- increase category number -->
-                    <?php $catNum++;?>
-
-                <!-- end the while loop -->
-                <?php endwhile ?> 
                 
     </div><!-- end row -->
 </div><!-- end container -->
