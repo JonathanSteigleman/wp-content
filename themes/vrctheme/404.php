9a0f2ca9e6ad0715507ef534b3ac285479c5c6ab
JaxonHarvey <42556138+JaxonHarvey@users.noreply.github.com>
2021-03-25 12:51:25 -0500

    Crated a line for reporting the broken link or page

--- themes/vrctheme/404.php
+++ themes/vrctheme/404.php
@@ -15,6 +15,7 @@ get_header(); ?>
 			<div class="page-wrapper">
 				<div class="page-content">
 					<h2><?php _e( 'This is somewhat embarrassing, isn’t it?'); ?></h2>
+                    <p><?php _e ( 'Could you please contact us below and notify us the broken page or link?' ); ?></p>
 					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?' ); ?></p>
 
 					<?php get_search_form(); ?>
