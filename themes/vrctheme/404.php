27a502c53c60643249277b0a9065d8aa5ec1bff7
JaxonHarvey <42556138+JaxonHarvey@users.noreply.github.com>
2021-04-04 19:49:30 -0500

    Added some Documentation to the 404 Page

--- themes/vrctheme/404.php
+++ themes/vrctheme/404.php
@@ -8,17 +8,17 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 
-			<header class="page-header">
+			<header class="page-header"> <!-- Title Header, Top of page -->
 				<h1 class="page-title"><?php _e( '404 Page Not Found'); ?></h1>
 			</header>
 
 			<div class="page-wrapper">
-				<div class="page-content">
+				<div class="page-content"> <!-- This class is the Three lines of text under the Title, (one is header to be bolded) -->
 					<h3><?php _e( 'It looks like nothing was found at this location. Maybe try a search?'); ?></h3>
                     <p><?php _e ( 'Could you please contact us below and notify us the broken page or link?' ); ?></p>
 					<p><?php _e( "This is embarrassing, isn't it?" ); ?></p>
 
-					<?php get_search_form(); ?>
+					<?php get_search_form(); ?> <!-- Allows for them to make a search onm the 404 Page -->
 				</div><!-- .page-content -->
 			</div><!-- .page-wrapper -->
 
