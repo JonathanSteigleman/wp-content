5eb4d5ff9c0381ef7067f8cbfafb173767dbdce7
kalicrockett <71088446+kalicrockett@users.noreply.github.com>
2021-04-10 14:04:09 -0500

    removed unused js

--- themes/vrctheme/js/main.js
+++ themes/vrctheme/js/main.js
@@ -2,17 +2,4 @@
 // File for our team's JavaScript Functions
 /////////////////////////////////////////////////////////////
 
-// Show the mobile navigation menu when you click on the icon
-// Contributer: Kali
-function showMobileNav() {
-    var x = document.getElementById("responsiveMenu");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
-
-
-
 
